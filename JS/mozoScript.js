5aee22dfea3d2dbefe174d15b785d9424e4795d4
juanma.io <juanma.ibarra.jmi@gmail.com>
2020-09-30 21:01:48 -0300

    acordion comida bebida

--- JS/mozoScript.js
+++ JS/mozoScript.js
@@ -1,3 +1,53 @@
+
+
+function mostrarBebidas(){
+  console.log('lasbebidas')
+}
+function popUp(){
+
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+function popUp2(){
+
+  var popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show");
+  const div = document.getElementsByTagName("body");
+    for (var i = 0; i < div.length; i++) {
+      div[i].addEventListener('click', cerrarPopup);
+  }
+}
+
+function cerrarPopup(){
+  var popup = document.getElementById("myPopup2");
+  if(popup.classList.contains("show")){
+    popup.classList.remove("show");
+  }
+}
+
+var accordionItemHeaders = document.querySelectorAll(".accordion_item_header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {        
+        var currentlyActiveAccordionItemHeader = document.querySelector(".accordion_item_header.active");
+        if (currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader !== accordionItemHeader ) {
+            currentlyActiveAccordionItemHeader.classList.toggle("active");
+            currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+        }
+
+        accordionItemHeader.classList.toggle("active");
+        var accordionItemBody = accordionItemHeader.nextElementSibling;
+        if(accordionItemHeader.classList.contains("active")){
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        }
+        else{
+            accordionItemBody.style.maxHeight = 0;
+        }
+    });
+});
+
+
 var foodsText = document.querySelector(".title-text .foods");
 var foodsForm = document.querySelector("form.foods");
 var foodsBtn = document.querySelector("label.foods");
@@ -15,17 +65,3 @@ drinksLink.onclick = (()=>{
   drinksBtn.click();
   return false;
 });
-
-function mostrarBebidas(){
-  console.log('lasbebidas')
-}
-function popUp(){
-
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-}
-function popUp2(){
-
-  var popup = document.getElementById("myPopup2");
-  popup.classList.toggle("show");
-}
